dfb5383b04089b24e9748f344d65d1be6019bfa4
kristadoc23 <123026546+kristadoc23@users.noreply.github.com>
2023-01-25 15:57:14 -0500

    Add files via upload

---
+++ lab 1.py
@@ -0,0 +1,50 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#our own notes
+print("Hello, world")
+Hello, world
+print"Hello, world"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
+print("Welcome to CPSC 207")
+Welcome to CPSC 207
+print("Hello\nWorld")
+Hello
+World
+print("How\nare\nyou?")
+How
+are
+you?
+print("Today", "is", "Wednesday")
+Today is Wednesday
+print("Today" + "is" + "Wednesday")
+TodayisWednesday
+print("123\n45\n6")
+123
+45
+6
+(1+2)*3
+9
+7/3
+2.3333333333333335
+7//3
+2
+7%3
+1
+22/4
+5.5
+22//4
+5
+3**2
+9
+2**3
+8
+6%2
+0
+(1+2)*3**4
+243
+print("2+2=", 2+2)
+2+2= 4
+print("50 pounds is", 50*0.453592, "Kilograms")
+50 pounds is 22.6796 Kilograms
+
+================== RESTART: C:\Users\kcheek01\Desktop\lab1.py ==================
